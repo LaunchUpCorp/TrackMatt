15d1b553b553f114b4120ab6b079258838134c6d
mjx <mattheweoro@gmail.com>
2023-09-12 17:26:36 -0400

    feat: Create landing page component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { config, GluestackUIProvider, Text, Box } from '@gluestack-ui/themed';
+import Landing from './pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config.theme}>
+      <Landing />
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ frontend/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import { Text, Box } from '@gluestack-ui/themed';
+
+export default function Landing() {
+  return (
+    <Box h='100%' w='100%' justifyContent='center' alignItems='center'>
+      <Text>Welcome to TrackMatt</Text>
+      <Text>Image Here</Text>
+      <Text>Create Account</Text>
+      <Text>Sign In</Text>
+    </Box>
+  );
+}

054f9a5f3507c212933f71e1e2abe8cfdb002778
MjxOro <mattheweoro@gmail.com>
2023-09-15 14:12:15 -0400

    refactor: add theme provider to App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@tamagui/core/reset.css";
+import { TamaguiProvider } from "tamagui";
+import config from "./tamagui.config";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config}>{/* your app here */}</TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

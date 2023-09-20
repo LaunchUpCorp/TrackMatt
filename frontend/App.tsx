909a1961b7430a96621fc1c106efd8e6dd857e38
MjxOro <mattheweoro@gmail.com>
2023-09-20 13:43:06 -0400

    chore: import Signup page to App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,7 @@
 import { TamaguiProvider } from 'tamagui';
 import config from './tamagui.config';
 import Landing from './pages/Landing';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
